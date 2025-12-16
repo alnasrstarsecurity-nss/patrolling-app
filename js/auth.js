7015e07c1600cc7db5d1bbd3afa848adb24be5a7
alnasrstarsecurity <administrator@alnasrstar.com>
2025-12-16 10:09:20 +0300

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,12 @@
+const SCRIPT_URL = "PASTE_YOUR_WEB_APP_URL";
+
+function requireLogin() {
+  if (!localStorage.getItem("user") || !localStorage.getItem("pass")) {
+    location.href = "index.html";
+  }
+}
+
+function logout() {
+  localStorage.clear();
+  location.href = "index.html";
+}
