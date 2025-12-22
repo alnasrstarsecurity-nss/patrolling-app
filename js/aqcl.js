8480e44acac8721c8eca347980274a5a134b57a4
alnasrstarsecurity <administrator@alnasrstar.com>
2025-12-22 17:01:41 +0300

    Update aqcl.js

--- js/aqcl.js
+++ js/aqcl.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
 /* ===============================
    CONFIG
 ================================ */
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw5IO2SGmEV2kjfiM-tScS7uofxeEMiy-YgC9tzpBLRlgOPS6NrvODZDJUskVQptcKFpA/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx8lnQh5Y1NfP_GWPI5IvRN_SjWqKesDYwd0kklW0zB8R0U5RXmTCLoPeE9T9TBMh4W/exec";
 
 const form = document.getElementById("aqclForm");
 const status = document.getElementById("status");
