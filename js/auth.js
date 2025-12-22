e4beedab3470bf8964eed1963dbb74546e7d96d5
alnasrstarsecurity <administrator@alnasrstar.com>
2025-12-22 14:05:51 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxo0juNCqbu-1WewP0pdAxEjX4r0IPdvzmAb2Rbtk399KiRR8aE7dpHd-nrawc4OFllQg/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyeMaRFMCO6AlTFwpGix_Pq4w6EQQfCm_TjLErgqwdaSQj29PuxTymAAULYVzhjX9efQg/exec";
 
 function requireLogin() {
   if (!localStorage.getItem("user") || !localStorage.getItem("pass")) {
