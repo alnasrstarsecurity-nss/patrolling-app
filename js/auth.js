4402c2071467290023ea247caeff0a0e0963ceb0
alnasrstarsecurity-nss <alnasrstarit@gmail.com>
2025-12-25 11:00:53 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzbpXIi3cAUbEesls5ujfbpawZ7Dr1LepcOUJIs7bgUNT6IHTczBrsNgVC0EWxLEYyGaA/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzH6ptt43Fv26IDmKdE3lClFy8OMc2zj-u0JJhFKOpMcTEZLuEwKP5ydth_yJMQ82VnjA/exec";
 
 function requireLogin() {
   if (!localStorage.getItem("user") || !localStorage.getItem("pass")) {
