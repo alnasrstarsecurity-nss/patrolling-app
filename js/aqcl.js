6122bf3c91bade4c7317edc4d1a175aa60e2758a
alnasrstarsecurity-nss <alnasrstarit@gmail.com>
2026-01-01 10:56:47 +0300

    Update aqcl.js

--- js/aqcl.js
+++ js/aqcl.js
@@ -144,7 +144,7 @@ function initSignaturePad(canvasId) {
 }
 
 // Initialize ONE signature pad
-const clearAqclSignature = initSignaturePad("sign");
+const clearAqclSignature = initSignaturePad("sign", "submitBtn");
 
 // Expose clear button function if you have one
 window.clearAqclSignature = clearAqclSignature;
